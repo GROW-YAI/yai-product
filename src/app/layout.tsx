886b42f59d774496fac59feab6113fa3091a6160
Ebenezer Yeboah <ebenezeryeboah46@gmail.com>
2025-09-30 11:09:58 +0000

    minor adds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,27 @@
-  import ThemeProvider from "./ThemeProvider";
-  import "@fontsource/playfair-display"; 
-  import "@fontsource/poppins"; 
-  import "./globals.css";
+import ThemeProvider from "./ThemeProvider";
+import "@fontsource/playfair-display";
+import "@fontsource/poppins";
+import "./globals.css";
 
-  export const metadata = {
-    title: "Styles By MakG",
-  
-    icons: {
-      icon: "/logo.png",
-      shortcut: "/logo.png",
-      apple: "/logo.png",
-    },
-  };
+export const metadata = {
+  title: "Styles By MakG",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
+};
 
-  export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <head>
-        <link rel="icon" type="image/png"  sizes="192x192" href="/logo.png" />
-      
-      </head>
-        <body>
-          <ThemeProvider>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    );
-  }
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
